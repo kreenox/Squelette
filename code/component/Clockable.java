6e2614b4f0dc23065477dd86c48c2b8d62166dcb
Fabien ANXA <jeangatien.cordier@gmail.com>
2016-06-23 12:17:37 +0200

    ajout de l'interface clockable pour les connections aux horloges

---
+++ code/component/Clockable.java
@@ -0,0 +1,13 @@
+package component;
+
+/**
+ * permet de rendre une classe connectable à une horloge
+ * @author Fabien ANXA
+ * @version 0.0.0
+ * @since 0.0.0
+ *
+ */
+public interface Clockable {
+	
+	public abstract void work(); 
+}
