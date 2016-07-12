ec35c9213f87cea5526591f182e3f11dc7d69562
Fabien <jeangatien.cordier@gmail.com>
2016-07-12 19:37:47 +0200

    mise a jour des bus abstrait, ajout de fonction : les bus ne gerent qu'une donnée a la fois

--- code/bibliotheque/component/bus/AbsBus.java
+++ code/bibliotheque/component/bus/AbsBus.java
@@ -3,7 +3,9 @@
 
 import component.AbsComponent;
 /**
- * les bus permettent de gerer les communications entre les composants
+ * les bus permettent de gerer les communications entre les composants.
+ * le bus commence en etat d'attente tant qu'il n'est pas appelé par un composant il ne transmet aucune donné.
+ * une fois appelé, le bus transmet au prochain tick d'horloge.
  * @author Fabien ANXA
  * @version 0.1.0
  * @since 0.0.0
@@ -11,16 +13,51 @@ import component.AbsComponent;
  */
 public abstract class AbsBus extends AbsComponent {
 
-
+	private int data;
+	
+	/**
+	 * créé et initialise le bus
+	 */
+	public AbsBus()
+	{
+		data = 0x00000000;
+	}
+	//set
+	//get
+	/**
+	 * permet de récupérer les données transmise via le bus
+	 * renvoi une donnée eronné si le bus ne transmet pas.
+	 * @returnla donnée transmise
+	 */
+	public int getTransmitedData()
+	{
+		return data;
+	}
+	//action
+	/**
+	 * appelle le bus pour transmettre des donnés
+	 * @param dat le données a transmettre
+	 */
+	public void call(int dat)
+	{
+		data = dat;
+	}
+	/**
+	 * fait transiter les donnés par le bus
+	 */
+	public abstract void transmit();
+	//questions
 	/**
 	 * pour savoir si le bus est en cours d'utilisation
 	 * @return true si il est utilisé false sinon
 	 */
 	public abstract boolean isUsed();
 	/**
-	 * pour savoir si le bus est en train de transmettre
+	 * pour savoir si le bus est en train de transmettre.
+	 * la methode getTransmitedData renvoi une donnée eronné si le bus ne transmet pas.
 	 * @return true si le bus transmet des données, false sinon
 	 */
 	public abstract boolean isTransmiting();
+	//redefintion
 
 }
