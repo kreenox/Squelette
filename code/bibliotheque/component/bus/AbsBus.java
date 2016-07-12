99b3bde946f1fcbbabe45afa90cdbd7b9cb13e53
Fabien <jeangatien.cordier@gmail.com>
2016-07-12 17:42:58 +0200

    ajout de la methode istransmiting au bus

--- code/bibliotheque/component/bus/AbsBus.java
+++ code/bibliotheque/component/bus/AbsBus.java
@@ -5,7 +5,7 @@ import component.AbsComponent;
 /**
  * les bus permettent de gerer les communications entre les composants
  * @author Fabien ANXA
- * @version 0.0.0
+ * @version 0.1.0
  * @since 0.0.0
  *
  */
@@ -17,5 +17,10 @@ public abstract class AbsBus extends AbsComponent {
 	 * @return true si il est utilisé false sinon
 	 */
 	public abstract boolean isUsed();
+	/**
+	 * pour savoir si le bus est en train de transmettre
+	 * @return true si le bus transmet des données, false sinon
+	 */
+	public abstract boolean isTransmiting();
 
 }
