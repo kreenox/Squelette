c5b2744d7b33fde44d8c243efe906ac64b3d7588
Fabien <jeangatien.cordier@gmail.com>
2016-07-13 11:18:40 +0200

    ajout de la classe de memoire à lecture seule

---
+++ code/bibliotheque/component/memory/AbsReadOnlyMemory.java
@@ -0,0 +1,20 @@
+package component.memory;
+
+/**
+ * cette classe gére les mémoire à lecture seule
+ * @author Fabien ANXA
+ * @version 0.0.0
+ * @since 0.1.0
+ *
+ */
+public abstract class AbsReadOnlyMemory extends AbsMemory {
+
+
+	@Override
+	public void write(int val, int adr) throws WRException {
+		throw new WRException(null);
+
+	}
+
+
+}
