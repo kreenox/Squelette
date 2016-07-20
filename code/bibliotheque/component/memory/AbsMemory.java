0e060d4a36395637fb0196ac68983d322b6dd323
Fabien <jeangatien.cordier@gmail.com>
2016-07-20 11:35:56 +0200

    depreciation des commandes de absMemory(elle n'avaient rien a faire la de base)

--- code/bibliotheque/component/memory/AbsMemory.java
+++ code/bibliotheque/component/memory/AbsMemory.java
@@ -17,30 +17,37 @@ public abstract class AbsMemory extends AbsComponent {
 	/**
 	 * la memoire ne doit effectuer aucune action
 	 */
+	@Deprecated
 	public static final int NOOP = 0x0000;
 	/**
 	 * la memoire doit effecture une lecture
 	 */
+	@Deprecated
 	public static final int READ = 0x1000;
 	/**
 	 * la memoire doit effectuer une ecriture
 	 */
+	@Deprecated
 	public static final int WRITE = 0x2000;
 	/**
 	 * la memoire doit effectuer un reset
 	 */
+	@Deprecated
 	public static final int RST = 0x3000;
 	/**
 	 * envoi le contenu de la mémoire à un autre composant
 	 */
+	@Deprecated
 	public static final int SEND = 0x4000;
 	/**
 	 * la memoire à reçu une adresse invalide
 	 */
+	@Deprecated
 	public static final int ADROUT = 0x0001;
 	/**
 	 * indique la fin de la copie de la mémoire
 	 */
+	@Deprecated
 	public static final int CPEND = 0x0002;
 	
 	public AbsMemory()
@@ -64,6 +71,13 @@ public abstract class AbsMemory extends AbsComponent {
 	 * @throws WRException si on tente d'écrire à une adresse illegale ou si la mémoire est protégée en écriture
 	 */
 	public abstract void write(int val, int adr) throws WRException;
+	
+	/**
+	 * permet de mettre à vide le contenu d'une addresse
+	 * @param adr l'adresse a vider
+	 * @throws WRException si on tente de remettre à vide une adresse illegale ou si la mémoire est protégée en écriture
+	 */
+	public abstract void reset(int adr) throws WRException;
 	
 	//question
 	//redefinition
