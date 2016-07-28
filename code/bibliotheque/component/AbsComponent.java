2c9ff0d55dedd80abdef2bca5869f4ed45d52075
Fabien Anxa <jeangatien.cordier@gmail.com>
2016-07-28 10:43:42 +0200

    ajout d'une horloge interne aux composants

--- code/bibliotheque/component/AbsComponent.java
+++ code/bibliotheque/component/AbsComponent.java
@@ -18,17 +18,48 @@ public abstract class AbsComponent extends Observable implements Clockable{
 	 * une liste des éléments connéctés au composant
 	 */
 	private ArrayList<AbsComponent> connected;
+	private Clock inck;//horloge interne
 	
 	public AbsComponent()
 	{
+		inck = new Clock();
+		inck.connectTo(this);
 		connected = new ArrayList<AbsComponent>();
 	}
+	//set
+	/**
+	 * permet de synchroniser ce composant sur une autre horloge.
+	 * pour liberer ce composant de l'horloge externe il faut voir avec l'horloge maitresse
+	 * @param ck l'horloge externe pour ce composant
+	 */
+	public void SetExternalClock(Clock ck)
+	{
+		ck.enslave(ck);
+	}
+	//get
+	/**
+	 * permet de connaitre le type d'un composant
+	 * @return le type du composant
+	 */
+	abstract public ComponentTypes getComponentType();
 	/**
 	 * permet de recuperer la liste des composants connéctés à celui-ci
 	 * @return un iterator contenant les composants connectés a celui-ci
 	 */
 	public Iterator<AbsComponent> getConnectedList()
 	{return connected.iterator();}
+	/**
+	 * construit une interface graphique
+	 * @return un JPanel qui contient l'interfacage graphique du composant
+	 */
+	abstract public JPanel getUI();
+	/**
+	 * retourne l'horloge interne du composant
+	 * @return l'horloge du composant
+	 */
+	public Clock getInternalClock()
+	{return inck;}
+	//actions
 	/**
 	 * connecte un composant à celui-ci
 	 * @param c le composant à connecter
@@ -57,16 +88,7 @@ public abstract class AbsComponent extends Observable implements Clockable{
 			throw new NonConnectedException();
 		connected.remove(c);
 	}
-	/**
-	 * construit une interface graphique
-	 * @return un JPanel qui contient l'interfacage graphique du composant
-	 */
-	abstract public JPanel getUI();
-	/**
-	 * permet de connaitre le type d'un composant
-	 * @return le type du composant
-	 */
-	abstract public ComponentTypes getComponentType();
+	
 	
 	@Override
 	public boolean equals(Object o)
